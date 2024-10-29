858855e59c3e430ea960702cc9da4911f23e4e0c
AJ-DEVS <akhmadjon331@gmail.com>
2024-10-29 18:44:52 +0900

    img src change

--- loader.js
+++ loader.js
@@ -1,6 +1,8 @@
-'use client'
- 
+"use client";
+
 export default function myImageLoader({ src, width, quality }) {
-    if(src.startsWith("https://images.pexels.com")) return src
-  return `https://ajportfolio.org${src}?w=${width}&q=${quality || 75}`
+  if (src.startsWith("https://images.pexels.com")) return src;
+  return `ajsportfolio-ajs-projects-e9241afc.vercel.app${src}?w=${width}&q=${
+    quality || 75
+  }`;
 }
